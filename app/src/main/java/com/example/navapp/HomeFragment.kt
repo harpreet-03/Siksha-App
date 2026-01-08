c17fc8d04c923ef03afa59b3a1901bb386946de6
harpreetsingh <harpreet162004@gmail.com>
2026-01-08 22:56:16 +0530

    Siksha app

---
+++ app/src/main/java/com/example/navapp/HomeFragment.kt
@@ -0,0 +1,40 @@
+package com.example.navapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.navapp.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Set OnClickListener on the DSA card view
+        binding.dsaCardView.setOnClickListener {
+            val intent = Intent(activity, DsaActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
